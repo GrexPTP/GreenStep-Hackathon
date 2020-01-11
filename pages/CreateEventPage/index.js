962a79bd79041bf0d7b31648bf7cc86b768ef33e
Phat Phan Tan <46423166+phatptce130319@users.noreply.github.com>
2020-01-11 18:36:19 +0700

    Complete the detail page

--- pages/CreateEventPage/index.js
+++ pages/CreateEventPage/index.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import {View, Keyboard} from 'react-native'
-import { TextInput, RadioButton, Text } from 'react-native-paper';
+import {View, Keyboard, ScrollView} from 'react-native'
+import { TextInput, RadioButton, Text, Button } from 'react-native-paper';
 import DateTimePicker from '@react-native-community/datetimepicker';
 const CreateEventPage = () =>  {
     const [name, setName] = useState('')
@@ -9,30 +9,51 @@ const CreateEventPage = () =>  {
     const [type, setType] = useState('time')
     const [date, setDate] = useState(new Date())
     const [time, setTime] = useState(new Date())
+    const [dateEnd, setDateEnd] = useState(new Date())
+    const [timeEnd, setTimeEnd] = useState(new Date())
     const [dateShow, setDateShow] = useState(false)
     const [timeShow, setTimeShow] = useState(false)
+    const [dateShowEnd, setDateShowEnd] = useState(false)
+    const [timeShowEnd, setTimeShowEnd] = useState(false)
     const setDateInput = (dateValue, date) => {
         Keyboard.dismiss()
         const currentDate = dateValue || date 
+        setDateShow(false)
         setDate(currentDate)
-        setTimeout(() => {
-            setDateShow(false)
-          }, 250);
+
+
         
         
     }
     const setTimeInput = (dateValue, time) => {
         Keyboard.dismiss()
         const currentDate = dateValue || time 
+        setTimeShow(false)
         setTime(currentDate)
-        setTimeout(() => {
-            setTimeShow(false)
-          }, 250);
+
+        
+    }
+    const setDateEndInput = (dateValue, date) => {
+        Keyboard.dismiss()
+        const currentDate = dateValue || date 
+        setDateShowEnd(false)
+        setDateEnd(currentDate)
+
+
         
         
     }
+    const setTimeEndInput = (dateValue, time) => {
+        Keyboard.dismiss()
+        const currentDate = dateValue || time 
+        setTimeShowEnd(false)
+        setTimeEnd(currentDate)
+
+        
+    }
     return (
-    <View style={{flex: 1, padding: 25}}>
+        <ScrollView>
+            <View style={{flex: 1, padding: 25}}>
         <TextInput
         mode='outlined'
         label='Event Name'
@@ -57,6 +78,7 @@ const CreateEventPage = () =>  {
 
       </View>
       <RadioButton.Group>
+          <View style={{flexDirection: 'row', justifyContent:'center'}}>
           <View style={{flexDirection:'row'}}>
           <Text>Time</Text>
           <RadioButton
@@ -64,7 +86,7 @@ const CreateEventPage = () =>  {
           status={type === 'time' ? 'checked' : 'unchecked'}
           onPress={() => setType('time') }
         />
-            <Text>{date.toDateString()}</Text>
+            
           </View>
         <View style={{flexDirection:'row'}}>
         <Text>Step</Text>
@@ -74,24 +96,46 @@ const CreateEventPage = () =>  {
           onPress={() => setType('step') }
         />
         </View>
+          </View>
+          
       </RadioButton.Group>
 
       <TextInput
       mode='outlined'
-        label='Date'
-        value={date}
+        label='Start Date'
+        value={date.toDateString()}
         onFocus={() => setDateShow(true)}
       />
       {dateShow &&  <DateTimePicker  value={date} onChange={(event, dateValue) => setDateInput(dateValue, date)}/>}
 
       <TextInput
       mode='outlined'
-        label='Time'
-        value={time}
+        label='Start Time'
+        value={`${time.getHours().toString()}:${time.getMinutes().toString()}:${time.getSeconds().toString()}`}
         onFocus={() => setTimeShow(true)}
       />
       {timeShow && <DateTimePicker mode={'time'} value={time} onChange={(event, dateValue) => setTimeInput(dateValue, time)} />}
+
+
+      <TextInput
+      mode='outlined'
+        label='End Date'
+        value={dateEnd.toDateString()}
+        onFocus={() => setDateShowEnd(true)}
+      />
+      {dateShowEnd &&  <DateTimePicker  value={dateEnd} onChange={(event, dateValue) => setDateEndInput(dateValue, date)}/>}
+
+      <TextInput
+      mode='outlined'
+        label='End Time'
+        value={`${timeEnd.getHours().toString()}:${timeEnd.getMinutes().toString()}:${timeEnd.getSeconds().toString()}`}
+        onFocus={() => setTimeShowEnd(true)}
+      />
+      {timeShowEnd && <DateTimePicker mode={'time'} value={timeEnd} onChange={(event, dateValue) => setTimeEndInput(dateValue, time)} />}
       
     </View>
+    <Button>Create Event</Button>
+        </ScrollView>
+    
 )}
 export default CreateEventPage
